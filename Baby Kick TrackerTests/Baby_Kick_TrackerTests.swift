1a96e784ab1254d8ef44eadc979c9624c33599f9
Patrick Lockett <patricklockett@Patricks-MacBook-Pro.local>
2016-12-14 15:41:47 -0800

    Initial Commit

---
+++ Baby Kick TrackerTests/Baby_Kick_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Baby_Kick_TrackerTests.swift
+//  Baby Kick TrackerTests
+//
+//  Created by Patrick Lockett on 12/14/16.
+//  Copyright © 2016 Patrick Lockett. All rights reserved.
+//
+
+import XCTest
+@testable import Baby_Kick_Tracker
+
+class Baby_Kick_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
